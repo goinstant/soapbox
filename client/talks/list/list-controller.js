e63fe28cdd5969c1861b54d92d07622d5e6100d9
Matthew Creager <mattcreager@goinstant.com>
2014-06-09 17:18:50 -0300

    list talks

--- client/talks/list/list-controller.js
+++ client/talks/list/list-controller.js
@@ -2,6 +2,12 @@
 
 'use strict';
 
-module.exports = function ListCtrl(TalkModel) {
-  console.log('how bout neeew?')
+module.exports = function ListCtrl($scope, $goKey, $goUsers) {
+  $scope.talks = $goKey('talks/org/public').$sync();
+
+  $scope.talks.$on('ready', function() {
+    _.each($scope.talks.$omit(), function(talk, talkId) {
+      $scope.talks[talkId].author = $goUsers().$getUser(talk.author).$sync();
+    });
+  });
 };
