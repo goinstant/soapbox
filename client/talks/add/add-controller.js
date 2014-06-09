ffb6d5360beb2b04c0a3c681b7f0202d2273efb9
Matthew Creager <mattcreager@goinstant.com>
2014-06-09 17:00:32 -0300

    Add talk, talk detail live

--- client/talks/add/add-controller.js
+++ client/talks/add/add-controller.js
@@ -1,7 +1,32 @@
-/* global module */
+/* global module, _ */
 
 'use strict';
 
-module.exports = function AddCtrl() {
-  console.log('add an controller loaded');
+module.exports = function AddCtrl($scope, $goKey, $goUsers, $state) {
+  // Defaults
+  $scope.talkVisibility = 'public';
+
+  $scope.submitTalkForm = function(isValid) {
+    $scope.submitted = true;
+
+    if (!isValid) return;
+
+    $goUsers().$self().then(function(selfModel) {
+      var talks = $goKey('talks/org').$key($scope.talkVisibility);
+      var talkData = {
+        title: $scope.title,
+        description: $scope.description,
+        duration: $scope.duration,
+        deck: $scope.deck,
+        video: $scope.video,
+        author: selfModel.id
+      };
+
+      talks.$add(talkData).get('context').then(function(context) {
+        var talkId = _.last(context.addedKey.split('/'));
+
+        $state.go('talks.detail', { talkId: talkId });
+      });
+    });
+  };
 };
--- client/talks/detail/detail-controller.js
+++ client/talks/detail/detail-controller.js
@@ -2,6 +2,10 @@
 
 'use strict';
 
-module.exports = function DetailCtrl() {
+module.exports = function DetailCtrl($scope, $stateParams, $goKey, $goUsers) {
+  $scope.currentTalk = $goKey('talks/org/public').$key($stateParams.talkId).$sync();
 
+  $scope.currentTalk.$on('ready', function() {
+    $scope.currentTalk.author = $goUsers().$getUser($scope.currentTalk.author).$sync();
+  });
 };
